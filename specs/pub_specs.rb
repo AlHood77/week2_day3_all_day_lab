2d2f99654b5684bba36fa6d2063793d6c19a344a
Alan <alanhood77@gmail.com>
2020-04-08 11:23:12 +0100

    pub can have name test completed and passed

--- specs/pub_specs.rb
+++ specs/pub_specs.rb
@@ -3,16 +3,19 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../pub.rb')
+require_relative('../drink.rb')
 
 class TestPub < Minitest::Test
     
     def setup
-        @drink1 = Drink.new("Whiskey")
-        @drink2 = Drink.new("Wine")
-        @drink3 = Drink.new("Vodka")
+        @drink1 = Drink.new("Whiskey", 3)
+        @drink2 = Drink.new("Wine", 4)
+        @drink3 = Drink.new("Vodka", 2)
         @pub = Pub.new("Winchester", 500, [@drink1, @drink2, @drink3])
     end
 
-
+    def test_pub_can_have_name
+        assert_equal("Winchester", @pub.name)
+    end
     
 end
